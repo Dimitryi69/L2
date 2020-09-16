2ae6f38f277482556ace2b74c3322c7d6ef08bdf
Dmitriy <d.chaley@mail.ru>
2020-09-16 17:11:41 +0300

    1

--- app/src/main/java/com/example/l2/DateofBirth.java
+++ app/src/main/java/com/example/l2/DateofBirth.java
@@ -2,13 +2,50 @@ package com.example.l2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DateofBirth extends AppCompatActivity {
 
+    EditText day = findViewById(R.id.Textday);
+    EditText mounth = findViewById(R.id.Textmounth);
+    EditText year = findViewById(R.id.Textyear);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dateof_birth);
+    }
+    public void OnClickDate(View view)
+    {
+        Bundle arguments = getIntent().getExtras();
+        String UnitType = arguments.get("UnitType").toString();
+        String Fullname = arguments.get("Fullname").toString();
+
+        if(UnitType=="Listener")
+        {
+            Intent intent = new Intent(this, Register.class);
+            intent.putExtra("UnitType", UnitType);
+            intent.putExtra("FullName", Fullname);
+            intent.putExtra("Day", day.getText().toString());
+            intent.putExtra("Mounth", mounth.getText().toString());
+            intent.putExtra("Year", year.getText().toString());
+            startActivity(intent);
+        }
+        else
+        {
+            Intent intent = new Intent(this, StudentCourse.class);
+            intent.putExtra("UnitType", UnitType);
+            intent.putExtra("FullName", Fullname);
+            intent.putExtra("Day", day.getText().toString());
+            intent.putExtra("Mounth", mounth.getText().toString());
+            intent.putExtra("Year", year.getText().toString());
+            startActivity(intent);
+        }
+
     }
 }
--- app/src/main/java/com/example/l2/NameInput.java
+++ app/src/main/java/com/example/l2/NameInput.java
@@ -2,16 +2,30 @@ package com.example.l2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 public class NameInput extends AppCompatActivity {
     EditText ename = findViewById(R.id.Textname);
-    EditText esurname =
+    EditText esurname = findViewById(R.id.Textsurname);
+    EditText ethirdname = findViewById(R.id.Textthird);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_input);
+
     }
-    public void OnClickname()
+    public void OnClickname(View view)
+    {
+        Intent intent = new Intent(this, DateofBirth.class);
+        String FullName = esurname.getText().toString()+" "+ename.getText().toString()+" "+ethirdname.getText().toString();
+        intent.putExtra("Fullname", FullName);
+        Bundle arguments = getIntent().getExtras();
+        String UnitType = arguments.get("UnitType").toString();
+        intent.putExtra("UnitType", UnitType);
+        startActivity(intent);
+    }
 }
